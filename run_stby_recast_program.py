85dd7d968dfd68382d7b5f75f820d62c6d0adc9b
Jason <jsaustin@iastate.edu>
2021-06-11 22:37:46 -0500

    Fixed video path

--- run_stby_recast_program.py
+++ run_stby_recast_program.py
@@ -62,7 +62,7 @@ def download_video():
 	# if not os.path.isfile(filename):
 		# urllib.urlretrieve(url, filename)
 	if not os.path.exists('video/Touch_the_temple.mp4'):
-		urllib.urlretrieve(videoPath, "video/{}".format(filename))
+		urllib.urlretrieve(videoPath, "video/standby_video.mp4")
 
 
 def load_framebuffer():
