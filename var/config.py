c3a6ca90d10795a6c1580f8c54f41fa89dc463a9
RDDebug <jsaustin23@gmail.com>
2021-06-12 00:26:58 -0500

    Create config.py

---
+++ var/config.py
@@ -0,0 +1,5 @@
+switch_used = True  # if true, the script will assume a 2 way switch will connect GPIO 23 and 24 to ground
+video_enabled = False  # only used if switch_used is False
+rtmp_enabled = True  # if False, switch in position 1 will default to still/music
+use_temple_image = False  # if True, the still image used will be the Nauvoo temple
+music_enabled = True  # if True, music will be played along with the still image
