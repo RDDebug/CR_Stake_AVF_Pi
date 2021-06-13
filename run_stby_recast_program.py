265cfb1f4bf93678d4e95368b7bc5351c3a62ee8
Jason <jsaustin23@gmail.com>
2021-06-13 14:48:25 -0500

    fixed errors and updated gitignore

--- run_stby_recast_program.py
+++ run_stby_recast_program.py
@@ -16,62 +16,65 @@ switch = [Button(23), Button(24)]
 config = {}
 
 def rmtp_stream():
-	omxprocess = subprocess.Popen(['omxplayer', '-o', 'hdmi', config["rtmpPath"]],
-								  stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
-	while switch[0].is_pressed and omxprocess.poll() is None:
-		sleep(1)
-	if omxprocess.poll() is None:
-		omxprocess.stdin.write(b'q')
+    omxprocess = subprocess.Popen(['omxplayer', '-o', 'hdmi', config["rtmpPath"]],
+                                  stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
+    while switch[0].is_pressed and omxprocess.poll() is None:
+        sleep(1)
+    if omxprocess.poll() is None:
+        omxprocess.stdin.write(b'q')
 
 
 def still_music(control):
-	music_list = os.listdir("music")
-	while True:
-		random.shuffle(music_list)
-		for song in music_list:
-			song_path = os.path.join("music", song)
-			omxprocess = subprocess.Popen(['omxplayer', '-o', 'hdmi', song_path],
-										  stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
-			if config["switch_used"] is True:
-				while control.is_pressed and omxprocess.poll() is None:
-					sleep(1)
-			else:
-				while omxprocess.poll() is None:
-					sleep(1)
-			if omxprocess.poll() is None:
-				omxprocess.stdin.write(b'q')
-			if not switch[1].is_pressed:
-				break
-		if not control.is_pressed:
-			break
+    music_list = os.listdir("music")
+    while True:
+        random.shuffle(music_list)
+        for song in music_list:
+            song_path = os.path.join("music", song)
+            omxprocess = subprocess.Popen(['omxplayer', '-o', 'hdmi', song_path],
+                                          stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
+            if config["switch_used"] == "True":
+                while control.is_pressed and omxprocess.poll() is None:
+                    sleep(1)
+            else:
+                while omxprocess.poll() is None:
+                    sleep(1)
+            if omxprocess.poll() is None:
+                omxprocess.stdin.write(b'q')
+            if not switch[1].is_pressed:
+                break
+        if not control.is_pressed:
+            break
 
 
 def play_video():
-	omxprocess = subprocess.Popen(['omxplayer', '--adev', 'hdmi', 'video/{}'.format(config["video"]["title"]), '--loop', '-b'],
-								  stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
-	while not switch[0].is_pressed and not switch[1].is_pressed and omxprocess.poll() is None:
-		sleep(1)
-	if omxprocess.poll() is None:
-		omxprocess.stdin.write(b'q')
-	sleep(5)
+    video_config = eval(config["video"])
+    omxprocess = subprocess.Popen(['omxplayer', '--adev', 'hdmi', 'video/{}'.format(video_config["title"]), '--loop', '-b'],
+                                  stdin=subprocess.PIPE, stdout=None, stderr=None, bufsize=0)
+    while not switch[0].is_pressed and not switch[1].is_pressed and omxprocess.poll() is None:
+        sleep(1)
+    if omxprocess.poll() is None:
+        omxprocess.stdin.write(b'q')
+    sleep(5)
 
 
 def download_video():
-	if not os.path.exists('video/{}'.format(config["video"]["title"])):
-		print("Downloading video {}".format(config["video"]["title"]))
-		gdd.download_file_from_google_drive(config["video"]["drive_id"], 'video/{}'.format(config["video"]["title"]))
-		print("Download complete")
+    video_config = eval(config["video"])
+    if not os.path.exists('video/{}'.format(video_config["title"])):
+        print("Downloading video {}".format(video_config["title"]))
+        gdd.download_file_from_google_drive(video_config["drive_id"], 'video/{}'.format(video_config["title"]))
+        print("Download complete")
 
 
 def load_framebuffer():
-	if config["use_temple_image"] is True:
-		image = "temple.jpg"
-	else:
-		today = date.today().strftime("%m/%d/%y")
-		cur_season = config["seasons"][int(today[0:2])]
-		image = "{}stakecenter.jpg".format(cur_season)
+    if config["use_temple_image"] == "True":
+        image = "temple.jpg"
+    else:
+        seasons = eval(config["seasons"])
+        today = date.today().strftime("%m/%d/%y")
+        cur_season = seasons[int(today[0:2])]
+        image = "{}stakecenter.jpg".format(cur_season)
 
-	os.system('fbi -a --noverbose -T 1 -t 15 "images/{}"'.format(image))
+    os.system('fbi -a --noverbose -T 1 -t 15 "images/{}"'.format(image))
 
 
 def load_config():
@@ -104,12 +107,12 @@ def run_switch():
 	load_framebuffer()
 	while True:
 		if switch[0].is_pressed:  # RTMP server
-			if config["rmtp_enabled"] is True:
+			if config["rmtp_enabled"] == "True":
 				rmtp_stream()
 			else:
 				still_music(switch[0])
 		elif switch[1].is_pressed:  # Music Only
-			if config["music_enabled"] is True:
+			if config["music_enabled"] == "True":
 				still_music(switch[1])
 			else:
 				sleep(10)
@@ -118,15 +121,15 @@ def run_switch():
 
 
 def run_no_switch():
-	if config["video_enabled"] is True:
+	if config["video_enabled"] == "True":
 		print("Downloading video if missing")
 		download_video()
 	load_framebuffer()
 	while True:
-		if config["video_enabled"] is True:
+		if config["video_enabled"] == "True":
 			play_video()
 		else:
-			if config["music_enabled"] is True:
+			if config["music_enabled"] == "True":
 				still_music(switch[0])
 			else:
 				sleep(10)
@@ -139,7 +142,7 @@ if __name__ == '__main__':
 		print(cnt)
 		sleep(1)  # wait for the video drivers to start up so the frame buffer can be loaded
 	load_config()
-	if config["switch_used"] is True:
+	if config["switch_used"] == "True":
 		run_switch()
 	else:
 		run_no_switch()
